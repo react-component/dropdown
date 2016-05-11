c8427cc75cb971612e568c097cc73255d948269d
yanxiaowu <xiaowu.yan@sui.me>
2016-05-11 11:03:10 +0800

    Add placement top-center and bottom-center

--- src/placements.js
+++ src/placements.js
@@ -12,8 +12,20 @@ export const placements = {
     offset: [0, -4],
     targetOffset,
   },
+  topCenter: {
+    points: ['bc', 'tc'],
+    overflow: autoAdjustOverflow,
+    offset: [0, -4],
+    targetOffset,
+  },
   bottomLeft: {
     points: ['tl', 'bl'],
+    overflow: autoAdjustOverflow,
+    offset: [0, 4],
+    targetOffset,
+  },
+  bottomCenter: {
+    points: ['tc', 'bc'],
     overflow: autoAdjustOverflow,
     offset: [0, 4],
     targetOffset,
