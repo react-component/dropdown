react-component/dropdown

commit f6ff40373c5d7c412768f800c9271d712a7a9abd

fix onVisibleChange. Fixes #10

src/Dropdown.jsx
@@ -60,10 +60,10 @@ const Dropdown = React.createClass({
     };
   },
 
-  componentWillReceiveProps(props) {
-    if ('visible' in props) {
+  componentWillReceiveProps({ visible }) {
+    if (visible !== undefined) {
       this.setState({
-        visible: props.visible,
+        visible,
       });
     }
   },
@@ -71,24 +71,20 @@ const Dropdown = React.createClass({
   onClick(e) {
     const props = this.props;
     const overlayProps = props.overlay.props;
-    if (!('visible' in props)) {
-      this.setState({
-        visible: false,
-      });
-    }
+    this.onVisibleChange(false);
     if (overlayProps.onClick) {
       overlayProps.onClick(e);
     }
   },
 
-  onVisibleChange(v) {
+  onVisibleChange(visible) {
     const props = this.props;
     if (!('visible' in props)) {
       this.setState({
-        visible: v,
+        visible,
       });
     }
-    props.onVisibleChange(v);
+    props.onVisibleChange(visible);
   },
 
   getMenuElement() {
@@ -114,12 +110,14 @@ const Dropdown = React.createClass({
   },
 
   render() {
-    const { prefixCls, children,
+    const {
+      prefixCls, children,
       transitionName, animation,
       align, placement, getPopupContainer,
       showAction, hideAction,
       overlayClassName, overlayStyle,
-      trigger, ...otherProps } = this.props;
+      trigger, ...otherProps,
+    } = this.props;
     return (<Trigger
       {...otherProps}
       prefixCls={prefixCls}

examples/simple.js
@@ -9,6 +9,10 @@ function onSelect({ key }) {
   console.log(`${key} selected`);
 }
 
+function onVisibleChange(visible) {
+  console.log(visible);
+}
+
 const menu = (
   <Menu onSelect={onSelect}>
     <MenuItem disabled>disabled</MenuItem>
@@ -21,7 +25,12 @@ const menu = (
 ReactDOM.render(<div style={{ margin: 20 }}>
   <div style={{ height: 100 }}/>
   <div>
-    <Dropdown trigger={['click']} overlay={menu} animation="slide-up">
+    <Dropdown
+      trigger={['click']}
+      overlay={menu}
+      animation="slide-up"
+      onVisibleChange={onVisibleChange}
+    >
       <button style={{ width: 100 }}>open</button>
     </Dropdown>
   </div>

commit 9da609b8d510c7b8e4fe57f79c152f470e7c3ec8

Allow diff visible change between select item and other
fix issue ant-design/ant-design#2507 broken by f6ff403

src/Dropdown.jsx
@@ -71,20 +71,18 @@ const Dropdown = React.createClass({
   onClick(e) {
     const props = this.props;
     const overlayProps = props.overlay.props;
-    this.onVisibleChange(false);
+    this.onVisibleChange(false, e);
     if (overlayProps.onClick) {
       overlayProps.onClick(e);
     }
   },
 
-  onVisibleChange(visible) {
+  onVisibleChange(visible, e) {
     const props = this.props;
     if (!('visible' in props)) {
-      this.setState({
-        visible,
-      });
+      this.setState({ visible });
     }
-    props.onVisibleChange(visible);
+    props.onVisibleChange(visible, e);
   },
 
   getMenuElement() {