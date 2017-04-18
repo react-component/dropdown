0080c3471b755d41fc158f7509ffe21c446ed84b
Lawrence Dabir-Alai <lawrence.dabir@gmail.com>
2017-04-18 11:52:36 +0100

    Use prop-types package

--- src/Dropdown.jsx
+++ src/Dropdown.jsx
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import ReactDOM from 'react-dom';
 import Trigger from 'rc-trigger';
 import placements from './placements';
