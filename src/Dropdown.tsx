61386577a60ed5889ded55f10769f857efaedba9
Bharat Rawat <31788510+bcrazydreamer@users.noreply.github.com>
2023-11-17 12:20:36 +0545

    Expose type autoDestroy from @rc-component/trigger & fix test coverage (#233)
    * Expose type autoDestroy from @rc-component/trigger
    
    * [fix coverage]: updated basic test snapshot

--- src/Dropdown.tsx
+++ src/Dropdown.tsx
@@ -23,6 +23,7 @@ export interface DropdownProps
     | 'mouseLeaveDelay'
     | 'onPopupAlign'
     | 'builtinPlacements'
+    | 'autoDestroy'
   > {
   minOverlayWidthMatchTrigger?: boolean;
   arrow?: boolean;
